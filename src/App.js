1bdf10cf357593b3572a1039a28e4b20e37a20f6
sidraakram <akramsidra44@gmail.com>
2025-03-14 04:00:12 -1200

    as

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import GitWork from './components/GitWork';
+import Sidra from './components/Sidra';
 
 function App() {
   return (
     <div className="App">
       <GitWork/>
+      <Sidra/>
     </div>
   );
 }
---
+++ src/components/Sidra.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sidra = () => {
+  return (
+    <div>
+        <h1>Pakistan is our country</h1>
+    </div>
+  )
+}
+
+export default Sidra
